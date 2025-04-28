cf81acfb6cc30048ee903a2f6e0487fb634ffc3a
Abdulwalie00 <ariya.ab31@gmail.com>
2025-04-28 13:26:09 +0800

    Implement html and ts filter for pgo pages.

--- src/app/pages/pgo/gad/gad.component.ts
+++ src/app/pages/pgo/gad/gad.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EventManagementComponent} from "../../event-management/event-management.component";
+import {EventService} from '../../../services/event.service';
 
 @Component({
   selector: 'app-gad',
-  imports: [],
+    imports: [
+        EventManagementComponent
+    ],
   templateUrl: './gad.component.html',
   styleUrl: './gad.component.css'
 })
-export class GadComponent {
+export class GadComponent implements OnInit {
+
+  constructor(private eventService: EventService) {
+  }
+
+  ngOnInit() {
+    this.eventService.setFilterDivision('GAD');
+  }
 
 }
--- src/app/pages/pgo/plpp/plpp.component.ts
+++ src/app/pages/pgo/plpp/plpp.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EventService} from '../../../services/event.service';
+import {EventManagementComponent} from '../../event-management/event-management.component';
 
 @Component({
   selector: 'app-plpp',
-  imports: [],
+  imports: [
+    EventManagementComponent
+  ],
   templateUrl: './plpp.component.html',
   styleUrl: './plpp.component.css'
 })
-export class PlppComponent {
+export class PlppComponent implements OnInit {
+
+  constructor(private eventService: EventService) {
+  }
+
+  ngOnInit() {
+    this.eventService.setFilterDivision('PLPP');
+  }
 
 }
