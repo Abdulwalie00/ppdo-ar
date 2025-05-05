1f1f674a43204c981d0302b0d3c7f8fc2247a6e1
Abdulwalie00 <ariya.ab31@gmail.com>
2025-05-05 09:42:18 +0800

    Add a main-layout

---
+++ src/app/components/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from '../../header/header.component';
+import {RouterOutlet} from '@angular/router';
+import {SidebarComponent} from '../../sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+    SidebarComponent
+  ],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css'
+})
+export class MainLayoutComponent {
+
+}
