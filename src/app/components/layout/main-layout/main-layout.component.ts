d64b0c0b95eefe93eae5bfb174cbb2ad966ca145
Abdulwalie00 <ariya.ab31@gmail.com>
2025-05-06 13:55:59 +0800

    add starting animation for mainlayout

--- src/app/components/layout/main-layout/main-layout.component.ts
+++ src/app/components/layout/main-layout/main-layout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from '../../header/header.component';
 import {RouterOutlet} from '@angular/router';
 import {SidebarComponent} from '../../sidebar/sidebar.component';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-main-layout',
@@ -11,7 +12,15 @@ import {SidebarComponent} from '../../sidebar/sidebar.component';
     SidebarComponent
   ],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.css'
+  styleUrl: './main-layout.component.css',
+  animations: [
+    trigger('fadeInAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate('250ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+    ]),
+  ],
 })
 export class MainLayoutComponent {
 
