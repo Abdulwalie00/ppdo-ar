29535b9d98d5e79d2a3aaf6a3cb9a660bedcfb67
Abdulwalie00 <ariya.ab31@gmail.com>
2025-07-25 08:47:34 +0800

    Update the Default role to ROLE_USER

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -23,7 +23,7 @@ export class AddUserComponent implements OnInit {
     email: '',
     username: '',
     password: '', // Use a 'password' field, not 'passwordHash'
-    role: 'ROLE_EDITOR' as UserRole, // Default to a safe role
+    role: 'ROLE_USER' as UserRole, // Default to a safe role
     divisionId: null,
   };
 
